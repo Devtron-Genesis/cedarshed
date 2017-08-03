038efdf4e00e372e0bfbb26f0e3b0cb9dfe52152
Shahzad Anwar <shahzad.anwar@devtrongenesis.com>
2017-08-03 10:14:02 +0500

    Price formator css

--- sites/all/themes/bootstrap_sub/js/main.js
+++ sites/all/themes/bootstrap_sub/js/main.js
@@ -7,7 +7,7 @@ function onChooseRegion(){
 		}else{
 			thisop.hide();
 		}
-		
+
 	});
 }
 (jQuery)(function(){
@@ -16,15 +16,15 @@ function onChooseRegion(){
 		var src = (jQuery)(this).prop('src');
 	var name = src.replace(/^.*\/|\.png$/g, '');
 	if(checkalt == ''){
-			//var newstr = src.replace('/sites/default/files/', ""); 
+			//var newstr = src.replace('/sites/default/files/', "");
 			(jQuery)(this).attr('alt',name);
 		}
 	});
 	//(jQuery)('.carousel').carousel();
 	(jQuery)('.dropdown a').removeAttr( 'data-toggle' );
-	
-	
-	
+
+
+
 	//Check to see if the window is top if not then display button
 	(jQuery)(window).scroll(function(){
 		if ((jQuery)(this).scrollTop() > 100) {
@@ -33,7 +33,7 @@ function onChooseRegion(){
 			(jQuery)('.scrollToTop').fadeOut();
 		}
 	});
-	
+
 	//Click event to scroll to top
 	(jQuery)('.scrollToTop').click(function(){
 		(jQuery)('html, body').animate({scrollTop : 0},800);
@@ -44,8 +44,8 @@ function onChooseRegion(){
 	if(widthwin < 768){
 	  expandMenu();
 	}
-	
-	
+
+
 	// (jQuery)(window).scroll(function(){
 // 		var headerHeight = (jQuery)('.header').outerHeight();
 // 		if((jQuery)(window).scrollTop() >= headerHeight - 50) {
@@ -54,10 +54,10 @@ function onChooseRegion(){
 // 			(jQuery)('.header').removeClass('fixed');
 // 		}
 // 	});
-	
-	
+
+
 	//(jQuery)('.box a[href^="/' + location.pathname.split("/")[1] + '"]').parent().addClass('active');
-	
+
 });
 (jQuery)( window ).resize(function() {
 var widthwin = 0;
@@ -66,18 +66,18 @@ var widthwin = 0;
  	if(widthwin < 768){
 	  expandMenu();
 	}
-}); 
+});
 
 function expandMenu(){
 	(jQuery)('.dropdown-menu').hide();
-	
+
 	(jQuery)('.dropdown a').each(function(){
-		
-		
+
+
 		var target = (jQuery)(this).attr('data-target');
 		var hrefthis =(jQuery)(this).attr('href');
 		if(hrefthis != '#'){
-			
+
 		}
 		if(target){
 			if(hrefthis != '#'){
@@ -87,8 +87,15 @@ function expandMenu(){
 				});
 			}
 		}
-		
-		
+
+
 	});
 	//alert('aaa');
 }
+
+(function($) {
+	$(document).ready(function() {
+		$('.field-name-commerce-price .commerce-price-savings-formatter-list .price-label').text('Price Starting at:');
+		$('.field-name-commerce-price .commerce-price-savings-formatter-price .price-label').text('Price Online Starting at:');
+	});
+})(jQuery);
