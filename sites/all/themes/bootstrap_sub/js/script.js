677fc31efe88ff4e2f4d837dcf0b858aeabf300c
Shahzad Anwar <shahzad.anwar@devtrongenesis.com>
2017-09-08 09:34:45 +0500

    Latest Updates

--- sites/all/themes/bootstrap_sub/js/script.js
+++ sites/all/themes/bootstrap_sub/js/script.js
@@ -11,6 +11,28 @@
     $('select[id*="edit-attributes-field-product-colour"]').parent().append("<span class='selected-colour'></span>");
     $('.form-item-attributes-field-product-colour .selected-colour').css('background', $('select[id*="edit-attributes-field-product-colour"] option:selected').val());
     $(".col-md-2.col-sm-4.col-xs-6.box").matchHeight();
+    if($('div.form-item[class*="window"]').length != 0){
+      $('<div class="product-windows"><h3>Windows:</h3><select name="windows-seletctor"><option selected="selected">No Windows</option></select></div>').insertBefore('.commerce-add-to-cart .shipping');
+    }
+    $('div.form-item[class*="window"]').each(function(e){
+      $(this).hide();
+      var text = $(this).find('.commerce-product-title').text().replace('Title:', '').trim() + ' - ' + $(this).find('.field-name-commerce-price').text().trim();
+      var value = $(this).find('input').attr('name');
+      $('select[name="windows-seletctor"]').append('<option value="'+value+'">'+text+'</option>');
+      if($(this).find('input').is(':checked')) {
+        $('select[name="windows-seletctor"]').val($(this).find('input').attr('name'));
+      }
+    });
+    $('select[name="windows-seletctor"]').change(function(e){
+      var selected = $(this).val();
+      $('div.form-item[class*="window"] input').each(function(e){
+        if($(this).attr('name') == selected) {
+          $(this).prop('checked', true);
+        } else {
+          $(this).prop('checked', false);
+        }
+      });
+    });
     console.log('Document Ready');
   });
   $(document).ajaxComplete(function(e) {
